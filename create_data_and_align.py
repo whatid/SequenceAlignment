3d6180c8587ab748e4e67520744f276949143b3d
AdminFred <tehxdchip@gmail.com>
2015-02-17 23:55:11 -0600

    added method to generate random dna sequence

--- create_data_and_align.py
+++ create_data_and_align.py
@@ -1,9 +1,23 @@
 __author__ = 'fwirjo'
 
 import sys
+import random
 try:
-    length = sys.argv[1]
+    length = int(sys.argv[1])
 except IndexError:
     print "Usage: create_data_and_align.py <int>"
     sys.exit(1)
 
+# initialize basic random number generator
+random.seed()
+
+
+def generate(string_length):
+    dna = ""
+    for i in range(0, string_length):
+        dna += random.choice("ACGT")
+
+    print dna
+    return
+
+generate(length)
